6e920b4906b86c88dcc519acd8c615c83578a55d
rodrigoceloto <rodrigo.celoto@gmail.com>
2014-06-12 12:14:46 -0300

    third commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,22 +1,22 @@
-## The working directory must be UCI HAR Dataset, with its subdirectories test and train, like created by unzipping the file getdata-projectfiles-UCI HAR Dataset.zip
+## The working directory must be "UCI HAR Dataset" with its subdirectories test and train, like created by unzipping the file getdata-projectfiles-UCI HAR Dataset.zip
 
 Sys.setenv(LANG = "en")
 Sys.setlocale("LC_TIME", "en_US.UTF-8")
 
-##Features
+##Features and units
 features<-read.table("features.txt")
 colnames(features)<-c("colnumber","variablename")
 
-##Test dataset binded with activity
+##Test dataset
 test<-read.table("test/X_test.txt")
 
-##Trainning dataset binded with activity
+##Trainning dataset
 train<-read.table("train/X_train.txt")
 
-##Merges the training and the test sets to create one data set
+##Binds the training and the test sets to create one data set
 dataset<-rbind(test,train)
 
-##Appropriately labels the data set with descriptive variable names
+##Labels the data set with descriptive variable names
 colnames(dataset)<-features$variablename
 
 ##Extracts only the measurements on the mean and standard deviation for each measurement
@@ -24,30 +24,37 @@ features$selection<-grepl("mean()",features$variablename,fixed=TRUE) | grepl("st
 selectedcolumns<-as.vector(features[(features$selection==1),"variablename"])
 dataset<-dataset[,selectedcolumns]
 
-##Uses descriptive activity names to name the activities in the data set
-  ##Add activity collumn
+##Write a file for codebook (only for documentation purposes)
+features$units<-ifelse(grepl("Acc",features$variablename,fixed=TRUE)==1 | grepl("gravity",features$variablename,fixed=TRUE)==1,"g","radians/second")
+CodeBookList<-features[(features$selection==1),c("variablename","units")]
+vector<-paste("* ",CodeBookList$variablename," / ",CodeBookList$units)
+write.table(vector,file="CodeBookList.txt",sep=",",row.names=FALSE,quote=FALSE)
+
+
+##Add a column with descriptive activity names in the data set
+  ##Add activity collumn to the dataset
   testlabels<-read.table("test/y_test.txt")
   trainlabels<-read.table("train/y_train.txt")
   labels<-rbind(testlabels,trainlabels)
   dataset$activitycode<-as.factor(labels$V1)
 
-  ##Add activity labels
+  ##Add activity labels (name)
   activity<-read.table("activity_labels.txt")
   colnames(activity)<-c("activitycode","activityname")
   dataset<-merge(dataset,activity,by.x="activitycode",by.y="activitycode",all=FALSE)
 
-##Add subjects
+##Add subjects column to the dataset
 testsubject<-read.table("test/subject_test.txt")
 trainsubject<-read.table("train/subject_train.txt")
 subject<-rbind(testsubject,trainsubject)
 dataset$subject<-as.factor(subject$V1)
 
-
-
-##Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+##Creates a  tidy dataset ("newdataset.txt") with the average of each variable b each activity and each subject
 meltdataset<-melt(dataset,id=c("activitycode","activityname","subject"),measure.vars=selectedcolumns)
 newdataset<-dcast(meltdataset,activitycode+activityname+subject ~ variable,mean)
 write.table(newdataset,file="newdataset.txt",sep=",",row.names=FALSE)
+
+
 
 
 
